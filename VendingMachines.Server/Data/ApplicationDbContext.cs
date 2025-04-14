57a51e8fdcac2c3f1f069efb79d049014943dd49
RanisKzn <ranis_2@mail.ru>
2025-04-14 13:08:14 +0300

    Добавьте файлы проекта. Добавил моделию

---
+++ VendingMachines.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using VendingMachines.Server.Models;
+
+namespace VendingMachines.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Coin> Coins { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(oi => oi.OrderId);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.Product)
+                .WithMany()
+                .HasForeignKey(oi => oi.ProductId);
+        }
+    }
+}
---
+++ VendingMachines.Server/Models/Brand.cs
@@ -0,0 +1,9 @@
+﻿namespace VendingMachines.Server.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
